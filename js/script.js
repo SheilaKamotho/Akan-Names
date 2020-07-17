90ab95c3ec38b363ec9df3253ee7cb6b68e2a796
Kamotho Sheila <kamothosheila@gmail.com>
2020-07-17 12:11:52 +0300

    add form variable

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 var date = document.getElementsByName ("date")
 var month = document.getElementsByName ("month")
+var year = document.getElementsByName ("year")
+var form = document.getElementsByTagName ("form")
+
+form.addEventListener
