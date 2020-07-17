429093cd9af2f33d5ce961818bb6db3e2a539ef1
Kamotho Sheila <kamothosheila@gmail.com>
2020-07-17 12:34:21 +0300

    add invalid month alert

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ form.addEventListener("submit",(e)); {
     let alert = []
     if (date.value < 1 || date.value > 31)
     alert ("Invalid date");
+    let alert = []
+    if (month.value < 1 || month.value > 12)
+    alert ("Invalid month");
     
 e.preventDefault ()
 }
