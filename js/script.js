072892927fa83a7e29313b0ebd339b2297bd0ef2
Kamotho Sheila <kamothosheila@gmail.com>
2020-07-17 15:14:24 +0300

    if or statements

--- js/script.js
+++ js/script.js
@@ -2,18 +2,45 @@ var date = document.getElementsByName ("date")
 var month = document.getElementsByName ("month")
 var year = document.getElementsByName ("year")
 var form = document.getElementsByTagName ("form")
+var gender = documrnt.getElementsByName ("gender")
 
-form.addEventListener("submit",(e)); {
-    let alert = []
     if (date.value < 1 || date.value > 31)
-    alert ("Invalid date");
-    let alert = []
+    console.log ("Invalid date");
     if (month.value < 1 || month.value > 12)
-    alert ("Invalid month");
-    
-e.preventDefault ()
+    console.log ("Invalid month");
+
+
+ var century = math.round((year)/100)
+ dayOfWeek = (((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day )%7
+
+ if (gender === male && dayOfWeek === 0) {
+     console.log("Your birth day was Sunday. Your name is Kwasi")
+ } else if (gender === male && dayOfWeek === 1) {
+    console.log ("Your birth day was Monday. Your name is Kwadwo")
+ } else if (gender === male && dayOfWeek === 2) {
+    console.log ("Your birth day was Tuesday. Your name is Kwabena")
+ }else if (gender === male && dayOfWeek === 3) {
+    console.log ("Your birth day was Wednesday. Your name is Kwaku")
+ }else if (gender === male && dayOfWeek === 4) {
+        console.log ("Your birth day was Thursday. Your name is Yaw")
+ } else if (gender === male && dayOfWeek === 5) {
+    console.log ("Your birth day was Friday. Your name is Kofi")
+ }else if (gender === male && dayOfWeek === 6) {
+    console.log ("Your birth day was Saturday. Your name is Kwame")
+ }
+
+ if (gender === female && dayOfWeek === 0) {
+    console.log("Your birth day was Sunday. Your name is Akosua")
+} else if (gender === male && dayOfWeek === 1) {
+   console.log ("Your birth day was Monday. Your name is Adwoa")
+} else if (gender === male && dayOfWeek === 2) {
+   console.log ("Your birth day was Tuesday. Your name is Abenaa")
+}else if (gender === male && dayOfWeek === 3) {
+   console.log ("Your birth day was Wednesday. Your name is Akua")
+}else if (gender === male && dayOfWeek === 4) {
+       console.log ("Your birth day was Thursday. Your name is Yaa")
+} else if (gender === male && dayOfWeek === 5) {
+   console.log ("Your birth day was Friday. Your name is Afua")
+}else if (gender === male && dayOfWeek === 6) {
+   console.log ("Your birth day was Saturday. Your name is Ama")
 }
-var CC = document.getElementsByPlaceHolder ("CC")
-var YY = document.getElementsByPlaceHolder ("YY")
-var MM = document.getElementsByPlaceHolder ("MM")
-var MM = document.getElementsByPlaceHolder ("MM")
